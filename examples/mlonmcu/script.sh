d7a09dcbd9dd606096fe555adbaa728219d76230
Philipp van Kempen <philipp.van-kempen@tum.de>
2024-07-20 05:40:28 +0200

    examples/mlonmcu/script.sh: update

--- examples/mlonmcu/script.sh
+++ examples/mlonmcu/script.sh
@@ -6,11 +6,19 @@ set -e
 #export MLONMCU_HOME=...
 ISAAC_SESSION=$(pwd)/sess
 
-# Run MLonMCU command
-python3 -m mlonmcu.cli.main flow run toycar --target etiss --backend tvmaotplus --feature-gen muriscvnnbyoc -c run.export_optional=1  -f log_instrs -c log_instrs.to_file=1 -c session.executor=process_pool -c runs_per_stage=0 -c etiss.compressed=0 -c etiss.atomic=0 -c etiss.fpu=none -c riscv_gcc.install_dir=$RISCV -c llvm.install_dir=$LLVM -c mlif.debug_symbols=1 -v -c mlif.toolchain=llvm -f memgraph_llvm_cdfg -f global_isel -v
-
 # Create ISAAC session
 python3 -m isaac_toolkit.session.create --session $ISAAC_SESSION --force
+python3 -m isaac_toolkit.utils.memgraph.purge_db --sess $ISAAC_SESSION
+
+# Run MLonMCU command
+# RV32: broken
+# python3 -m mlonmcu.cli.main flow run toycar --target etiss --backend tvmaotplus --feature-gen muriscvnnbyoc -c run.export_optional=1  -f log_instrs -c log_instrs.to_file=1 -c session.executor=process_pool -c runs_per_stage=0 -c etiss.compressed=0 -c etiss.atomic=0 -c etiss.fpu=none -c riscv_gcc.install_dir=$RISCV -c llvm.install_dir=$LLVM -c mlif.debug_symbols=1 -v -c mlif.toolchain=llvm -f memgraph_llvm_cdfg -f global_isel -v
+# RV64
+# python3 -m mlonmcu.cli.main flow run sine_model --target etiss --backend tvmaotplus -c run.export_optional=1 -c log_instrs.to_file=1 -c session.executor=process_pool -c runs_per_stage=0 -c etiss.compressed=0 -c etiss.xlen=64 -c etiss.atomic=0 -c etiss.fpu=double -c riscv_gcc.install_dir=$RISCV -c riscv_gcc.name=riscv64-unknown-elf -c llvm.install_dir=$LLVM -c mlif.debug_symbols=1 -v
+python3 -m mlonmcu.cli.main flow run toycar --target etiss --backend tvmaotplus -c run.export_optional=1  -f log_instrs -c log_instrs.to_file=1 -c session.executor=process_pool -c runs_per_stage=0 -c etiss.compressed=0 -c etiss.xlen=64 -c etiss.atomic=0 -c etiss.fpu=double -c riscv_gcc.install_dir=$RISCV -c riscv_gcc.name=riscv64-unknown-elf -c llvm.install_dir=$LLVM -c mlif.debug_symbols=1 -v -c mlif.toolchain=llvm
+# python3 -m mlonmcu.cli.main flow run toycar --target etiss --backend tvmaotplus --feature-gen muriscvnnbyoc -c run.export_optional=1  -f log_instrs -c log_instrs.to_file=1 -c session.executor=process_pool -c runs_per_stage=0 -c etiss.compressed=0 -c etiss.xlen=64 -c etiss.atomic=0 -c etiss.fpu=double -c riscv_gcc.install_dir=$RISCV -c riscv_gcc.name=riscv64-unknown-elf -c llvm.install_dir=$LLVM -c mlif.debug_symbols=1 -v -c mlif.toolchain=llvm
+# -f memgraph_llvm_cdfg -f global_isel -v
+
 
 # Load files
 ELF=$MLONMCU_HOME/temp/sessions/latest/runs/latest/generic_mlonmcu
@@ -25,6 +33,7 @@ python3 -m isaac_toolkit.frontend.memgraph.llvm_mir_cdfg --session sess -f
 python3 -m isaac_toolkit.analysis.static.dwarf --session $ISAAC_SESSION --force
 python3 -m isaac_toolkit.analysis.static.mem_footprint --session $ISAAC_SESSION --force
 python3 -m isaac_toolkit.analysis.dynamic.trace.basic_blocks --session $ISAAC_SESSION --force
+python3 -m isaac_toolkit.analysis.dynamic.trace.instr_operands --session $ISAAC_SESSION --force
 
 
 # Investigate artifacts
