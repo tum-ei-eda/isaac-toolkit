4b02f7e81af4159239ed0507dbc4ca1aa3f6b9c3
Philipp van Kempen <philipp.van-kempen@tum.de>
2024-07-20 05:48:41 +0200

    isaac_toolkit/utils/pickle_printer.py: fixes

--- isaac_toolkit/utils/pickle_printer.py
+++ isaac_toolkit/utils/pickle_printer.py
@@ -4,14 +4,36 @@ import pickle
 import pandas as pd
 
 
+# TODO: argparse (--mem, ...)
+
+def print_memory_footprint(df):
+    print("================")
+    print("Memory Footprint")
+    print("================")
+    mem = pd.concat([df.dtypes, df.memory_usage(deep=True)], axis=1).sort_values(1, ascending=False)
+    mem.rename(columns={0: "dtype", 1: "mem"}, inplace=True)
+    total = mem["mem"].sum()
+    print(mem)
+    print("TOTAL:", total)
+
+
 def main():
     assert len(sys.argv) == 2, "Unexpected number of arguments"
     file = sys.argv[1]
     with open(file, "rb") as f:
         data = pickle.load(f)
-    print("Unpickled Data:")
-    with pd.option_context("display.max_rows", None, "display.max_columns", None, "display.width", None, "max_colwidth", 150):
-        print(data)
+    # PRINT = False
+    PRINT = True
+    if PRINT:
+        print("Unpickled Data:")
+        with pd.option_context("display.max_rows", None, "display.max_columns", None, "display.width", None, "max_colwidth", 150):
+            print(data)
+
+    MEM = True
+    if MEM:
+       print_memory_footprint(data)
+
+
 
 if __name__ == "__main__":
     main()
