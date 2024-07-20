3136e5cf6b9ee85c92c0035bd345a12ff7f13183
Philipp van Kempen <philipp.van-kempen@tum.de>
2024-07-20 05:53:57 +0200

    add isaac_toolkit/utils/memgraph/purge_db.py

---
+++ isaac_toolkit/utils/memgraph/purge_db.py
@@ -0,0 +1,56 @@
+import sys
+import argparse
+from pathlib import Path
+
+from neo4j import GraphDatabase
+import networkx as nx
+from networkx.drawing.nx_agraph import write_dot
+import matplotlib.pyplot as plt
+
+from isaac_toolkit.session import Session
+from isaac_toolkit.session.artifact import ArtifactFlag, GraphArtifact
+
+
+
+
+def handle(args):
+    assert args.session is not None
+    session_dir = Path(args.session)
+    assert session_dir.is_dir(), f"Session dir does not exist: {session_dir}"
+    sess = Session.from_dir(session_dir)
+    override = args.force
+    memgraph_config = sess.config.memgraph
+    hostname = memgraph_config.hostname
+    port = memgraph_config.port
+    user = memgraph_config.user
+    password = memgraph_config.password
+    # TODO: database?
+
+    driver = GraphDatabase.driver(f"bolt://{hostname}:{port}", auth=(user, password))
+    query = """
+    MATCH (n) DETACH DELETE n;
+    """
+
+    results = driver.session().run(query)
+    print("results")
+
+
+def get_parser():
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "--log", default="info", choices=["critical", "error", "warning", "info", "debug"]
+    )  # TODO: move to defaults
+    parser.add_argument("--session", "--sess", "-s", type=str, required=True)
+    parser.add_argument("--force", "-f", action="store_true")
+    # TODO: allow overriding memgraph config?
+    return parser
+
+
+def main(argv):
+    parser = get_parser()
+    args = parser.parse_args(argv)
+    handle(args)
+
+
+if __name__ == "__main__":
+    main(sys.argv[1:])
