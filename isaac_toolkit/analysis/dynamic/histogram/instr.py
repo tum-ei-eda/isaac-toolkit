9f26a0ab371fbf9a14e28ccfd22f287a60daef65
Philipp van Kempen <philipp.van-kempen@tum.de>
2024-08-20 11:14:18 +0200

    implement instr hist

---
+++ isaac_toolkit/analysis/dynamic/histogram/instr.py
@@ -0,0 +1,88 @@
+import sys
+import logging
+import argparse
+from pathlib import Path
+from collections import defaultdict
+
+import pandas as pd
+
+from isaac_toolkit.session import Session
+from isaac_toolkit.session.artifact import ArtifactFlag, TableArtifact, filter_artifacts
+
+
+logging.basicConfig(level=logging.DEBUG)  # TODO
+logger = logging.getLogger(__name__)
+
+
+def collect_instructions(trace_df):
+    # print("trace_df", trace_df)
+    instrs = defaultdict(int)
+    for row in trace_df.itertuples(index=False):
+        # print("row", row)
+        instr = row.instr
+        instr = instr.strip()  # TODO: fix in frontend
+        instrs[instr] += 1
+    instrs_data = []
+    for instr_name, instr_count in instrs.items():
+        instr_data = {"instr": instr_name, "count": instr_count}
+        instrs_data.append(instr_data)
+    instrs_df = pd.DataFrame(instrs_data)
+    total_count = instrs_df["count"].sum()
+    instrs_df["rel_count"] = instrs_df["count"] / total_count
+    instrs_df.sort_values("count", ascending=False, inplace=True)
+
+    return instrs_df
+
+
+def create_instr_hist(sess: Session, force: bool = False):
+    artifacts = sess.artifacts
+    # print("artifacts", artifacts)
+    trace_artifacts = filter_artifacts(artifacts, lambda x: x.flags & ArtifactFlag.INSTR_TRACE)
+    # print("elf_artifacts", elf_artifacts)
+    assert len(trace_artifacts) == 1
+    trace_artifact = trace_artifacts[0]
+
+    instrs_df = collect_instructions(trace_artifact.df)
+    # print("operands_df", operands_df)
+
+    attrs = {
+        "trace": trace_artifact.name,
+        "kind": "histogram",
+        "by": __name__,
+    }
+
+    instrs_artifact = TableArtifact("instrs_hist", instrs_df, attrs=attrs)
+    # TODO:
+    # operands_hist_artifact = TableArtifact(f"instr_operands_hist", operands_hist_df, attrs=attrs2)
+    sess.add_artifact(instrs_artifact, override=force)
+    # sess.add_artifact(operands_hist_artifact, override=force)
+
+
+def handle(args):
+    assert args.session is not None
+    session_dir = Path(args.session)
+    assert session_dir.is_dir(), f"Session dir does not exist: {session_dir}"
+    sess = Session.from_dir(session_dir)
+    create_instr_hist(sess, force=args.force)
+    sess.save()
+
+
+def get_parser():
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "--log", default="info", choices=["critical", "error", "warning", "info", "debug"]
+    )  # TODO: move to defaults
+    parser.add_argument("--session", "--sess", "-s", type=str, required=True)
+    parser.add_argument("--force", "-f", action="store_true")
+    # TODO: allow overriding memgraph config?
+    return parser
+
+
+def main(argv):
+    parser = get_parser()
+    args = parser.parse_args(argv)
+    handle(args)
+
+
+if __name__ == "__main__":
+    main(sys.argv[1:])
