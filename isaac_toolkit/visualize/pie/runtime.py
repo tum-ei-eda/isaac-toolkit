9a49833bb93e8a86ebbf275e4de7acef775f714a
Philipp van Kempen <philipp.van-kempen@tum.de>
2024-11-19 13:43:02 +0100

    isaac_toolkit/visualize/pie/mem_footprint.py: handle missing optional artifacts

--- isaac_toolkit/visualize/pie/runtime.py
+++ isaac_toolkit/visualize/pie/runtime.py
@@ -74,23 +74,29 @@ def create_runtime_pie_plots(
     symbol_map_artifacts = filter_artifacts(
         artifacts, lambda x: x.flags & ArtifactFlag.TABLE and x.name == "symbol_map"
     )
-    assert len(symbol_map_artifacts) == 1
-    symbol_map_artifact = symbol_map_artifacts[0]
-    symbol_map_df = symbol_map_artifact.df
+    symbol_map_df = None
+    if len(symbol_map_artifacts) > 0:
+        assert len(symbol_map_artifacts) == 1
+        symbol_map_artifact = symbol_map_artifacts[0]
+        symbol_map_df = symbol_map_artifact.df
 
     instrs_hist_artifacts = filter_artifacts(
         artifacts, lambda x: x.flags & ArtifactFlag.TABLE and x.name == "instrs_hist"
     )
-    assert len(instrs_hist_artifacts) == 1
-    instrs_hist_artifact = instrs_hist_artifacts[0]
-    instrs_hist_df = instrs_hist_artifact.df
+    instrs_hist_df = None
+    if len(instrs_hist_artifacts) > 0:
+        assert len(instrs_hist_artifacts) == 1
+        instrs_hist_artifact = instrs_hist_artifacts[0]
+        instrs_hist_df = instrs_hist_artifact.df
 
     opcodes_hist_artifacts = filter_artifacts(
         artifacts, lambda x: x.flags & ArtifactFlag.TABLE and x.name == "opcodes_hist"
     )
-    assert len(opcodes_hist_artifacts) == 1
-    opcodes_hist_artifact = opcodes_hist_artifacts[0]
-    opcodes_hist_df = opcodes_hist_artifact.df
+    opcodes_hist_df = None
+    if len(opcodes_hist_artifacts) > 0:
+        assert len(opcodes_hist_artifacts) == 1
+        opcodes_hist_artifact = opcodes_hist_artifacts[0]
+        opcodes_hist_df = opcodes_hist_artifact.df
 
     plots_dir = sess.directory / "plots"
     plots_dir.mkdir(exist_ok=True)
