729ffb4cbd53d337812f1390a7f0db4e35e7bc77
Philipp van Kempen <philipp.van-kempen@tum.de>
2024-12-09 10:16:50 +0100

    add license headers

--- tests/unit-tests/session/test_artifact.py
+++ tests/unit-tests/session/test_artifact.py
@@ -1,3 +1,21 @@
+#
+# Copyright (c) 2024 TUM Department of Electrical and Computer Engineering.
+#
+# This file is part of ISAAC Toolkit.
+# See https://github.com/tum-ei-eda/isaac-toolkit.git for further info.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
 import pytest
 
 from isaac_toolkit.session.artifact import (
@@ -23,7 +41,7 @@ ALL_ARTIFACTS = [FOO_ARTIFACT, BAR_ARTIFACT, BAZ_ARTIFACT]
         (ALL_ARTIFACTS, lambda x: x.attrs.get("key") == "val", [BAR_ARTIFACT]),
         (ALL_ARTIFACTS, lambda x: x.flags & ArtifactFlag.PYTHON, ALL_ARTIFACTS),
         (ALL_ARTIFACTS, lambda x: x.flags & ArtifactFlag.TABLE, [BAZ_ARTIFACT]),
-        ([], lambda x: name in ["foo", "bar", "baz"], []),
+        ([], lambda x: x.name in ["foo", "bar", "baz"], []),
     ],
 )
 def test_artifact_filter_artifacts(artifacts, func, expected):
