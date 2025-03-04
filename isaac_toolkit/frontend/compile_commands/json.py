3d9bb10d1c48fbc39f728a9a2ee21487cec28ef3
Philipp van Kempen <philipp.van-kempen@tum.de>
2025-03-04 09:25:36 +0100

    add isaac_toolkit/frontend/compile_commands/

---
+++ isaac_toolkit/frontend/compile_commands/json.py
@@ -0,0 +1,83 @@
+#
+# Copyright (c) 2024 TUM Department of Electrical and Computer Engineering.
+#
+# This file is part of ISAAC Toolkit.
+# See https://github.com/tum-ei-eda/isaac-toolkit.git for further info.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+import sys
+import json
+import argparse
+from pathlib import Path
+
+import pandas as pd
+
+from isaac_toolkit.session import Session
+from isaac_toolkit.session.artifact import TableArtifact
+
+
+def load_compile_commands_json(sess: Session, input_file: Path, force: bool = False):
+    assert input_file.is_file()
+    name = input_file.name
+    attrs = {
+        "kind": "compile_commands",
+        "by": __name__,
+    }
+    with open(input_file, "r") as f:
+        content = f.read()
+
+    data = json.loads(content)
+
+    new = []
+
+    for entry in data:
+        new.append(entry)
+
+    compile_commands_df = pd.DataFrame(new)
+
+    artifact = TableArtifact(name, compile_commands_df, attrs=attrs)
+    sess.add_artifact(artifact, override=force)
+
+
+def handle(args):
+    assert args.session is not None
+    session_dir = Path(args.session)
+    assert session_dir.is_dir(), f"Session dir does not exist: {session_dir}"
+    sess = Session.from_dir(session_dir)
+    input_file = Path(args.file)
+    load_compile_commands_json(sess, input_file, force=args.force)
+    sess.save()
+
+
+def get_parser():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("file")
+    parser.add_argument(
+        "--log",
+        default="info",
+        choices=["critical", "error", "warning", "info", "debug"],
+    )  # TODO: move to defaults
+    parser.add_argument("--session", "--sess", "-s", type=str, required=True)
+    parser.add_argument("--force", "-f", action="store_true")
+    return parser
+
+
+def main(argv):
+    parser = get_parser()
+    args = parser.parse_args(argv)
+    handle(args)
+
+
+if __name__ == "__main__":
+    main(sys.argv[1:])
