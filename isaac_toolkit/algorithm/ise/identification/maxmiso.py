b08d88b9c7ac9d34e9a7c62d82c56f7272beebfa
Philipp van Kempen <philipp.van-kempen@tum.de>
2025-03-28 10:30:20 +0100

    isaac_toolkit/algorithm/ise/identification/maxmiso.py: update

--- isaac_toolkit/algorithm/ise/identification/maxmiso.py
+++ isaac_toolkit/algorithm/ise/identification/maxmiso.py
@@ -30,35 +30,38 @@ from isaac_toolkit.session.artifact import ArtifactFlag, GraphArtifact, filter_a
 
 
 def maxmiso_algo(G):
-    print("algo")
-    print("G", G, dir(G))
+    # print("algo")
+    # print("G", G, dir(G))
+    G = G.copy()
     if G.number_of_nodes() == 0:
-        return
+        return []
     mapping = dict(zip(G.nodes.keys(), range(len(G.nodes))))
     G = nx.relabel_nodes(G, mapping)
     topo = list(reversed(list(nx.topological_sort(G))))
-    print("topo", topo)
+    # print("topo", topo)
     invalid = [False] * len(topo)
     fanout = [0] * len(topo)
     processed = [False] * len(topo)
     fanout_org = [0] * len(topo)
     for node in G.nodes:
-        print("node", node)
-        print("node_data", G.nodes[node])
+        # print("node", node)
+        # print("node_data", G.nodes[node])
         op_type = G.nodes[node]["properties"].get("op_type", "ot")
-        print("op_type", op_type)
+        # print("op_type", op_type)
         od = G.out_degree(node)
         # is_output = od == 0
         is_output = op_type == "output"
         if is_output:
             od += 1
-        print("od", od)
+        # print("od", od)
         fanout_org[topo.index(node)] = od
         # is_input = G.nodes[node].get("label") == "Const"  # TODO: add to db
         is_input = op_type == "input"
+        is_const = op_type == "constant"
+        is_label = op_type == "label"
         load_instrs = ["LB", "LBU", "LH", "LHU", "LW"]  # TODO: do not hardcode
         is_load = G.nodes[node].get("label") in load_instrs  # TODO: add to db
-        is_invalid = is_input or is_load
+        is_invalid = is_input or is_load or is_const or is_label
         # is_invalid = False
         if is_invalid:
             invalid[topo.index(node)] = True
@@ -93,70 +96,70 @@ def maxmiso_algo(G):
         if size > 1:
 
             def calc_inputs(max_miso):
-                print("calc_inputs", max_miso)
+                # print("calc_inputs", max_miso)
                 inputs = [False] * len(topo)
                 ret = 0
                 max_miso_nodes = [topo[i] for i, val in enumerate(max_miso) if val]
-                print("mmn", max_miso_nodes)
+                # print("mmn", max_miso_nodes)
                 for node in max_miso_nodes:
-                    print("node", node)
+                    # print("node", node)
                     ins = G.in_edges(node)
-                    print("ins", ins)
+                    # print("ins", ins)
                     for in_ in ins:
-                        print("in_", in_, G.nodes[in_[0]].get("label"))
+                        # print("in_", in_, G.nodes[in_[0]].get("label"))
                         src = in_[0]
-                        if (
-                            not max_miso[topo.index(src)]
-                            and not inputs[topo.index(src)]
-                        ):
+                        if not max_miso[topo.index(src)] and not inputs[topo.index(src)]:
                             ret += 1
                             inputs[topo.index(src)] = True
-                print("ret", ret)
+                # print("ret", ret)
                 # input("1")
                 return ret
 
             def calc_outputs(max_miso):
-                print("calc_outputs", max_miso)
+                # print("calc_outputs", max_miso)
                 # outputs = [False] * len(topo)
                 ret = 0
                 max_miso_nodes = [topo[i] for i, val in enumerate(max_miso) if val]
-                print("mmn", max_miso_nodes)
+                # print("mmn", max_miso_nodes)
                 for node in max_miso_nodes:
-                    print("node", node)
+                    # print("node", node)
                     if G.nodes[node]["properties"]["op_type"] == "output":
-                        print("A")
+                        # print("A")
                         ret += 1
                     else:
-                        print("B")
+                        # print("B")
                         outs = G.out_edges(node)
-                        print("outs", outs)
+                        # print("outs", outs)
                         for out_ in outs:
-                            print("out_", out_, G.nodes[out_[0]].get("label"))
+                            # print("out_", out_, G.nodes[out_[0]].get("label"))
                             dst = out_[1]
                             if not max_miso[topo.index(dst)]:
                                 ret += 1
-                print("ret", ret)
+                # print("ret", ret)
                 # input("1")
                 return ret
 
             num_inputs = calc_inputs(max_miso)
-            print("num_inputs", num_inputs)
+            # print("num_inputs", num_inputs)
             num_outputs = calc_outputs(max_miso)
-            print("num_outputs", num_outputs)
-            input(">")
+            # print("num_outputs", num_outputs)
+            # input(">")
             max_misos.append(max_miso)
-    print("max_misos", max_misos, len(max_misos))
+    # print("max_misos", max_misos, len(max_misos))
     from itertools import compress
 
     max_misos_ = [list(compress(topo, max_miso)) for max_miso in max_misos]
-    print("max_misos_", max_misos_)
+    # print("max_misos_", max_misos_)
     # max_misos__ = [nx.subgraph_view(G, filter_node=lambda node: max_miso[topo.index(node)]) for max_miso in max_misos]
     # max_misos__ = [nx.subgraph_view(G, filter_node=lambda node: node in max_miso) for max_miso in max_misos_]
     # max_misos__ = [nx.subgraph_view(G, filter_node=lambda node: node in max_miso) for max_miso in max_misos_]
     # print("G", type(G), G)
     # print("G.", type(G.subgraph(max_misos_[0])), G.subgraph(max_misos_[0])))
     max_misos__ = [G.subgraph(max_miso) for max_miso in max_misos_]
-    print("max_misos__", max_misos__)
+    reverse_mapping = {v: k for k, v in mapping.items()}
+    G = nx.relabel_nodes(G, reverse_mapping)
+    max_misos__ = [nx.relabel_nodes(max_miso, reverse_mapping) for max_miso in max_misos__]
+    # print("max_misos__", max_misos__)
     # for i, mig in enumerate(max_misos__):
     #     print("i,mig", i, mig)
     #     write_dot(mig, f"maxmiso{i}.dot")
@@ -201,14 +204,8 @@ def handle(args):
             # TODO: module_name
             view = nx.subgraph_view(
                 G,
-                filter_node=lambda node: (
-                    bb_name is None
-                    or G.nodes[node]["properties"].get("basic_block") == bb_name
-                )
-                and (
-                    func_name is None
-                    or G.nodes[node]["properties"].get("func_name") == func_name
-                )
+                filter_node=lambda node: (bb_name is None or G.nodes[node]["properties"].get("basic_block") == bb_name)
+                and (func_name is None or G.nodes[node]["properties"].get("func_name") == func_name)
                 and "%bb" not in G.nodes[node].get("label"),
             )
             G_ = G.subgraph([node for node in view.nodes])
