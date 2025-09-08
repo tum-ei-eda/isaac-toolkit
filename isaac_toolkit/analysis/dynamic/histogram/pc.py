51730e6e1e663d1dcf897d70488370f8abaa0224
Philipp van Kempen <philipp.van-kempen@tum.de>
2025-09-08 09:28:56 +0200

    isaac_toolkit/analysis/dynamic/histogram/pc.py: implement

--- isaac_toolkit/analysis/dynamic/histogram/pc.py
+++ isaac_toolkit/analysis/dynamic/histogram/pc.py
@@ -16,3 +16,84 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 #
+import sys
+import logging
+import argparse
+from pathlib import Path
+
+# from collections import defaultdict
+
+import pandas as pd
+
+from isaac_toolkit.session import Session
+from isaac_toolkit.session.artifact import ArtifactFlag, TableArtifact, filter_artifacts
+
+
+logging.basicConfig(level=logging.DEBUG)  # TODO
+logger = logging.getLogger(__name__)
+
+
+def collect_pcs(trace_df):
+    pcs_df = (
+        trace_df["pc"]
+        .value_counts()
+        .rename_axis("pc")       # makes 'pc' a column
+        .reset_index(name="count")  # turns counts into a column
+    )
+    total_count = pcs_df["count"].sum()
+    pcs_df["rel_count"] = pcs_df["count"] / total_count
+    pcs_df.sort_values("count", ascending=False, inplace=True)
+    return pcs_df
+
+
+def create_pc_hist(sess: Session, force: bool = False):
+    artifacts = sess.artifacts
+    # print("artifacts", artifacts)
+    trace_artifacts = filter_artifacts(artifacts, lambda x: x.flags & ArtifactFlag.INSTR_TRACE)
+    # print("elf_artifacts", elf_artifacts)
+    assert len(trace_artifacts) == 1
+    trace_artifact = trace_artifacts[0]
+
+    pcs_df = collect_pcs(trace_artifact.df)
+    # print("operands_df", operands_df)
+
+    attrs = {
+        "trace": trace_artifact.name,
+        "kind": "histogram",
+        "by": __name__,
+    }
+
+    pcs_artifact = TableArtifact("pcs_hist", pcs_df, attrs=attrs)
+    sess.add_artifact(pcs_artifact, override=force)
+
+
+def handle(args):
+    assert args.session is not None
+    session_dir = Path(args.session)
+    assert session_dir.is_dir(), f"Session dir does not exist: {session_dir}"
+    sess = Session.from_dir(session_dir)
+    create_pc_hist(sess, force=args.force)
+    sess.save()
+
+
+def get_parser():
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "--log",
+        default="info",
+        choices=["critical", "error", "warning", "info", "debug"],
+    )  # TODO: move to defaults
+    parser.add_argument("--session", "--sess", "-s", type=str, required=True)
+    parser.add_argument("--force", "-f", action="store_true")
+    # TODO: allow overriding memgraph config?
+    return parser
+
+
+def main(argv):
+    parser = get_parser()
+    args = parser.parse_args(argv)
+    handle(args)
+
+
+if __name__ == "__main__":
+    main(sys.argv[1:])
