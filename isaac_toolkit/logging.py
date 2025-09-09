bf700b2a878f4cf59d8ce3cd6152f77ec244e457
Philipp van Kempen <philipp.van-kempen@tum.de>
2025-09-09 07:34:18 +0200

    isaac_toolkit/logging.py: hide numexpr warnings

--- isaac_toolkit/logging.py
+++ isaac_toolkit/logging.py
@@ -27,6 +27,7 @@ logging.basicConfig(
     format="[%(asctime)s]::%(levelname)s - %(message)s",
     datefmt="%Y-%m-%d %H:%M:%S",
 )
+logging.getLogger("numexpr").setLevel(logging.WARNING)
 
 initialized = False
 
