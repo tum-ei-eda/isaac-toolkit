9dff86ee5e87a03409f2273d550f1f07dfaabd2b
Philipp van Kempen <philipp.van-kempen@tum.de>
2025-10-28 13:57:52 +0100

    lint

--- isaac_toolkit/frontend/instr_trace/riscv_qemu.py
+++ isaac_toolkit/frontend/instr_trace/riscv_qemu.py
@@ -5,18 +5,21 @@ input_file = "/workspaces/isaac-toolkit/examples/standalone/coremark/log"
 
 dfs = []
 # 'with' context works in pandas >= 1.4 for TextFileReader
-with pd.read_csv(input_file, sep="@",
-                 header=None,
-                 chunksize=2**22,  # ~4 million lines per chunk
-                 # chunksize=2**20,  # ~4 million lines per chunk
-                 engine="python") as reader:
+with pd.read_csv(
+    input_file,
+    sep="@",
+    header=None,
+    chunksize=2**22,  # ~4 million lines per chunk
+    # chunksize=2**20,  # ~4 million lines per chunk
+    engine="python",
+) as reader:
     for df in tqdm(reader, disable=False):
         # print("A", df.head())
         # df is a DataFrame chunk
         # e.g., extract PCs here
         pcs_chunk = df[0].str.extract(r"\[(?:[^/]+/){1}([^/]+)/")[0].apply(lambda x: int(x, 16))
         pcs_chunk = pcs_chunk.astype("category")
-        
+
         print("B", pcs_chunk.head(), len(pcs_chunk), pcs_chunk.dtypes, pcs_chunk.memory_usage())
         # process pcs_chunk or append to list
         dfs.append(pcs_chunk)
