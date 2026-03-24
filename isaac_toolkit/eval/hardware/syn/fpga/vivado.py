238cd8f7d4bd38c756630a6cbdb3298a586245f5
Philipp van Kempen <philipp.van-kempen@tum.de>
2026-03-24 10:06:39 +0100

    fix license headers

--- isaac_toolkit/eval/hardware/syn/fpga/vivado.py
+++ isaac_toolkit/eval/hardware/syn/fpga/vivado.py
@@ -1,5 +1,5 @@
 #
-# Copyright (c) 2024 TUM Department of Electrical and Computer Engineering.
+# Copyright (c) 2026 TUM Department of Electrical and Computer Engineering.
 #
 # This file is part of ISAAC Toolkit.
 # See https://github.com/tum-ei-eda/isaac-toolkit.git for further info.
@@ -16,7 +16,6 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 #
-# # import os
 import sys
 import shutil
 import subprocess
